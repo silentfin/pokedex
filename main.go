2639d2e5ea51acc21831b83242066de2b22e4db3
silentfin <152957041+silentfin@users.noreply.github.com>
2026-08-15 22:29:38 +0530

    refactor code

--- main.go
+++ main.go
@@ -1,30 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {
-			break
-		}
-		input := scanner.Text()
-		actualInput := cleanInput(input)
-		commandName := actualInput[0]
-		command, ok := getCommands()[commandName]
-		if !ok {
-			fmt.Println("Unknown command")
-		} else {
-			err := command.callback()
-			if err != nil {
-				fmt.Println(err)
-			}
-			continue
-		}
-	}
+	configs := config{commands: getCommands()}
+	repl(&configs)
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -9,7 +10,15 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	commands map[string]cliCommand
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
 }
 
 func getCommands() map[string]cliCommand {
@@ -26,22 +35,43 @@ func getCommands() map[string]cliCommand {
 		},
 	}
 }
-func commandHelp() error {
+
+func commandHelp(configs *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Printf("Usage:\n\n")
-	allCommands := getCommands()
-	for _, command := range allCommands {
+	for _, command := range configs.commands {
 		fmt.Printf("%s: %s\n", command.name, command.description)
 	}
 	return nil
 }
 
-func cleanInput(text string) []string {
-	return strings.Fields(strings.ToLower(text))
-}
-
-func commandExit() error {
+func commandExit(configs *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
+}
+
+func repl(configs *config) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		actualInput := cleanInput(input)
+		if len(actualInput) == 0 {
+			continue
+		}
+		commandName := actualInput[0]
+		command, ok := configs.commands[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+		} else {
+			err := command.callback(configs)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
