bfec7a67fee049b766c7faca9cc35a223e04f243
silentfin <152957041+silentfin@users.noreply.github.com>
2026-08-31 23:02:23 +0530

    add cache

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -2,8 +2,12 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"time"
+
+	"github.com/silentfin/pokedex/internal/pokecache"
 )
 
 type PokedexLocationArea struct {
@@ -16,16 +20,29 @@ type PokedexLocationArea struct {
 	} `json:"results"`
 }
 
-func GetMapAreas(url string) (PokedexLocationArea, error) {
-	res, err := http.Get(url)
-	if err != nil {
-		return PokedexLocationArea{}, err
-	}
-	defer res.Body.Close()
+var apiCache = pokecache.NewCache(5 * time.Second)
 
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return PokedexLocationArea{}, err
+func GetMapAreas(url string) (PokedexLocationArea, error) {
+	var data []byte
+	cachedData, found := apiCache.Get(url)
+	if found {
+		data = cachedData
+	} else {
+		res, err := http.Get(url)
+		if err != nil {
+			return PokedexLocationArea{}, err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode == http.StatusOK {
+			data, err = io.ReadAll(res.Body)
+			if err != nil {
+				return PokedexLocationArea{}, err
+			}
+			apiCache.Add(url, data)
+		} else {
+			return PokedexLocationArea{}, fmt.Errorf("request failed: %d", res.StatusCode)
+		}
 	}
 
 	areas := PokedexLocationArea{}
